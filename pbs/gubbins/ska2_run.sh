b459ac5f8a0e0d967e11ef0089c40c8a51ab094e
jdaeth274 <joshdaeth@yahoo.co.uk>
2024-04-18 21:55:42 +0100

    finessing ska2

--- pbs/gubbins/ska2_run.sh
+++ pbs/gubbins/ska2_run.sh
@@ -1,18 +1,22 @@
 #PBS -l select=1:ncpus=16:mem=32gb
 #PBS -l walltime=48:00:00
-
+#PBS -J 1-6
 ## Script to run SKA 2 on the Imperial HPC 
 
 eval "$(/rds/general/user/jd2117/home/miniforge3/bin/conda shell.bash hook)"
 
-source /rds/general/user/jd2117/home/miniforge3/bin/activate panaroo_env
+source /rds/general/user/jd2117/home/miniforge3/bin/activate ska2_env
 
+CURRENT_FILE=$(head -n $PBS_ARRAY_INDEX /rds/general/user/jd2117/home/acba_legion_2024/ska_input/acba_files_references.txt | tail -n 1)
+FILE_LIST=$(echo $CURRENT_FILE | awk '{print $1}')
+REFERENCE_LOC=$(echo $CURRENT_FILE | awk '{print $2}')
 
-FILE_LIST="~/acba_legion_2024/ska_input/gc2_5092_paths.txt"
-export REFERENCE_LOC="~/acba_legion_2024/ska_input/GCA_001573125.1_ASM157312v1_genomic.fna" 
+export $REFERENCE_LOC 
 
-mkdir -p ~/../ephemeral/acba_legion/ska2_res/GC2/
-cd ~/../ephemeral/acba_legion/ska2_res/GC2/
+OUTPUT_NAME=$(basename $FILE_LIST | sed 's/\..*$/_ska_res/g')
+
+mkdir -p "~/../ephemeral/acba_legion/ska2_res/${OUTPUT_NAME}/"
+cd "~/../ephemeral/acba_legion/ska2_res/${OUTPUT_NAME}/"
 
 START_TIME=$SECONDS
 cat ${FILE_LIST} | parallel -j 16 "NGSID=\$(basename {} | sed 's/_g.*fna/_temp_skf/g');
