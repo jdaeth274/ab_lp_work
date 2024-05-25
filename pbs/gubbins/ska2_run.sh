781a5dd53bd29236d30204d90888a791da2ea8a4
jdaeth274 <joshdaeth@yahoo.co.uk>
2024-05-25 10:49:40 +0100

    removing parallel runs in GC2 isolates SKA2

--- pbs/gubbins/ska2_run.sh
+++ pbs/gubbins/ska2_run.sh
@@ -1,38 +1,29 @@
-#PBS -l select=1:ncpus=16:mem=32gb
-#PBS -l walltime=48:00:00
-#PBS -J 1-6
-## Script to run SKA 2 on the Imperial HPC 
-
-eval "$(/rds/general/user/jd2117/home/miniforge3/bin/conda shell.bash hook)"
+#PBS -l select=1:ncpus=1:mem=16gb
+#PBS -l walltime=24:00:00
+#PBS -J 1-10
+## Script to run SKA 2 on the Imperial HPC for the acba gc2 lineage
 
 source /rds/general/user/jd2117/home/miniforge3/bin/activate ska2_env
 
-CURRENT_FILE=$(head -n $PBS_ARRAY_INDEX /rds/general/user/jd2117/home/acba_legion_2024/ska_input/acba_files_references.txt | tail -n 1)
-FILE_LIST=$(echo $CURRENT_FILE | awk '{print $1}')
+CURRENT_FILE=$(head -n $PBS_ARRAY_INDEX /rds/general/user/jd2117/home/acba_legion_2024/ska_input/gc2_splits.txt | tail -n 1)
 
-export REFERENCE_LOC=$(echo $CURRENT_FILE | awk '{print $2}') 
+REFERENCE_LOC="/rds/general/user/jd2117/home/acba_legion_2024/ska_input/GCA_001573125.1_ASM157312v1_genomic.fna"
 
-OUTPUT_NAME=$(basename $FILE_LIST | sed 's/\..*$/_ska_res/g')
-
-echo "This is the current file"
-echo $CURRENT_FILE
-echo "This is the file list:"
-echo $FILE_LIST
-echo "This is the reference loc:"
-echo $REFERENCE_LOC
-echo "This is the output name:"
-echo $OUTPUT_NAME
+OUTPUT_NAME="GC2_SKA2_RES"
 
 mkdir -p "~/../ephemeral/acba_legion/ska2_res/${OUTPUT_NAME}/"
 cd "~/../ephemeral/acba_legion/ska2_res/${OUTPUT_NAME}/"
 
 START_TIME=$SECONDS
-cat ${FILE_LIST} | parallel -j 16 "NGSID=\$(basename {} | sed 's/_g.*fna/_temp_skf/g');
- ska build -o \$NGSID -k 31 {} ;
- ska weed --ambig-mask \${NGSID}.skf;
-MAPOUT=\$(basename {} | sed 's/_g.*fna/_aln/g');
- ska map $REFERENCE_LOC \${NGSID}.skf -o \${MAPOUT}.aln"
+cat ${CURRENT_FILE} | while read line;
+do
+NGSID=$(basename $line | sed 's/_g.*fna/_temp_skf/g')
+ska build -o $NGSID -k 31 $line ;
+ska weed --ambig-mask ${NGSID}.skf;
+MAPOUT=$(basename $line | sed 's/_g.*fna/_aln/g');
+ska map $REFERENCE_LOC ${NGSID}.skf -o ${MAPOUT}.aln
 
+done
 END=$(( SECONDS - START_TIME ))
 echo "Finished in ${END} (s) "
 
